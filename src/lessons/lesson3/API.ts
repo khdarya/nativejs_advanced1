48acef4ff63bed0aaea218116e7f847185163250
khdarya <darya.khakhlova@gmail.com>
2020-11-07 21:46:11 -0500

    lesson13(5adv) this call apply bind

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const configOMB = {
     baseURL: 'http://www.omdbapi.com',
 };
-const key = '';
+const key = 'cad0dcef';
 const axiosInstance = axios.create(configOMB);
 
 const API = {
